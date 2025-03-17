2c3d286e1bdf4b25a1390da7e5d9b79af9bc00dd
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2025-03-17 12:12:02 +0530

    somechanges

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Nodemon is working!");
+console.log("Changes are being watched and the server is restarting automatically.");
+console.log("This is a test message to see if nodemon is working correctly.");
